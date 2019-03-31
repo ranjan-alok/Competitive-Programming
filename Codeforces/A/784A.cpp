bed4c9b81fa8cfa6f32b90c332699bd0adae7216
Alok Ranjan <ranjanalokmfp@gmail.com>
2019-03-31 12:32:32 +0530

    Add files via upload

---
+++ Codeforces/A/784A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long
+
+int32_t main()
+{
+	IOS;
+    int a;
+    int arr[] = {4, 22, 27, 58, 85, 94, 121, 166, 202, 265, 274, 319, 346, 355, 378, 382, 391, 438, 454, 483, 517, 526, 535, 562, 576, 588, 627, 634, 636, 645};
+    cin >> a;
+    cout << arr[a-1] << "\n";
+	return 0;
+}
---
+++ Codeforces/A/952A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long
+
+int32_t main()
+{
+	IOS;
+    int a;
+    cin >> a;
+    bitset <16> binary(a);
+    cout <<binary[0] << endl;
+	return 0;
+}
